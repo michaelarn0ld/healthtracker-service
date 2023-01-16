f7d509eb1023533c549152d7a70f53a051538c4b
Michael Arnold <me@michaelarnold.io>
2023-01-15 23:37:44 -0800

    secure posts with key

--- src/main/java/io/michaelarnold/healthtracker/config/ApplicationConfiguration.java
+++ src/main/java/io/michaelarnold/healthtracker/config/ApplicationConfiguration.java
@@ -4,6 +4,8 @@ import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.AWSSecretsManagerClientBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +15,21 @@ import javax.validation.Validator;
 @Configuration
 public class ApplicationConfiguration {
 
-    public static final String HEALTH_TRACKER_PRIMARY_KEY_NAME = "exerciseType";
+    public static final String HEALTH_TRACKER_POST_KEY = "accessPin";
     public static final String HEALTH_TRACKER_TABLE_NAME = "HealthTracker";
     private static final Regions REGION = Regions.US_WEST_1;
     private static final ProfileCredentialsProvider CREDENTIALS = new ProfileCredentialsProvider();
     @Bean
     public AmazonDynamoDB initializeAmazonDynamoDB() {
         return AmazonDynamoDBClientBuilder.standard()
+                .withRegion(REGION)
+                .withCredentials(CREDENTIALS)
+                .build();
+    }
+
+    @Bean
+    public AWSSecretsManager initializeAmazonSecretsManager() {
+        return AWSSecretsManagerClientBuilder.standard()
                 .withRegion(REGION)
                 .withCredentials(CREDENTIALS)
                 .build();
--- src/main/java/io/michaelarnold/healthtracker/controller/ExerciseDataPointController.java
+++ src/main/java/io/michaelarnold/healthtracker/controller/ExerciseDataPointController.java
@@ -1,26 +1,51 @@
 package io.michaelarnold.healthtracker.controller;
 
+import com.amazonaws.services.secretsmanager.AWSSecretsManager;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueRequest;
+import com.amazonaws.services.secretsmanager.model.GetSecretValueResult;
 import io.michaelarnold.healthtracker.domain.ExerciseDataPointService;
 import io.michaelarnold.healthtracker.domain.Result;
 import io.michaelarnold.healthtracker.model.ExerciseDataPoint;
+import lombok.extern.log4j.Log4j2;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import static io.michaelarnold.healthtracker.config.ApplicationConfiguration.HEALTH_TRACKER_POST_KEY;
+
 
 @RestController
+@Log4j2
 public class ExerciseDataPointController {
 
     @Autowired
     ExerciseDataPointService service;
 
-    @PostMapping("/exercisedatapoint")
-    public ResponseEntity<?> addExerciseDataPoint(@RequestBody @Valid ExerciseDataPoint exerciseDataPoint) {
+    @Autowired
+    AWSSecretsManager awsSecretsManager;
+
+    @PostMapping("/exerciseDataPoint")
+    public ResponseEntity<?> addExerciseDataPoint(@RequestBody @Valid ExerciseDataPoint exerciseDataPoint,
+                                                  @RequestParam(name = HEALTH_TRACKER_POST_KEY) String accessPin) {
+
+        GetSecretValueRequest request = new GetSecretValueRequest();
+        request.setSecretId(HEALTH_TRACKER_POST_KEY);
+        GetSecretValueResult secretResult = awsSecretsManager.getSecretValue(request);
+        String secret = secretResult.getSecretString();
+        JSONObject object = new JSONObject(secret);
+        String key = object.getString(HEALTH_TRACKER_POST_KEY);
+
+        if (!key.equals(accessPin)){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
         Result<ExerciseDataPoint> result = service.add(exerciseDataPoint);
         if (!result.isSuccess()) {
             return new ResponseEntity<>(result.getMessages(), HttpStatus.BAD_REQUEST);
